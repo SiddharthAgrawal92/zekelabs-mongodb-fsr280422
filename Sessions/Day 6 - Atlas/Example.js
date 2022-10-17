2e88adccd64a3274bad3f05786c5370e85d7ed9e
SiddharthAgrawal92 <47270428+SiddharthAgrawal92@users.noreply.github.com>
2022-10-17 21:41:54 +0530

    Day 6- Atlas

---
+++ Sessions/Day 6 - Atlas/Example.js
@@ -0,0 +1,68 @@
+
+
+//Sales View --> Login --> Dashboard (Angular)
+//backend
+
+//User/Customer View --> Login --> Dashboard, User Detail.. etc (RectJS)
+
+//www.example.com
+//Sales - folder
+//Customer - folder
+
+//www.api.example.com
+//api folder nodejs/java/python
+//api2 folder java apis
+
+// api 1-5 running service in nodejs
+// api 6-10 running service in java spring boot
+
+//www.admin.example.com
+
+//static/css, images
+// chunks-13816n381638164318n64.js //login
+// chunks-13816n381638164318n64.js //dashboard
+// main.js
+//index.html
+
+//login.jsx
+//dashboard.jsx
+
+//http://localhost:3000 dev sourceMaps=true
+
+//https://example.com - domain
+
+//https://example.com/login
+
+
+// attract more more and customer - first impression is the last impression
+// first page should load fast
+// how to bring customer back to our webpage again - customer experience and ease of use
+
+//Utility Services - Electricity bill is generated
+
+//SAAS - google docs - trial storage of 5GBs
+//IAAS - Virtual machine/VMWARE - 1TB
+//DAAS - Database AS A service -  scalablity - 1TB --> 5TB --> 1GB --> 1MB --> 5TB
+//Atlas
+
+//Atlas - Sharded Cluster
+
+//1TB
+//calls will be made to the single machine -->  performce will degrade
+
+//Case 1
+//[X]1TB - Collection
+// Single machine will be able to handle certain load -> Performance wil be slow 
+
+//Case 2
+//[Shard A]256Gb[Shard B]256GB[Shard C]256GB[Shard D]256GB - Collection
+// Multiple machines will give more performance
+
+//Case 3
+// [Replica Set A]1TB
+// [Replica Set B]1TB
+// [Replica Set C]1TB
+// Multiple Replica sets will give more performance for read
+
+//just like windows restore point there exists a back up restore point in mongodb cluster backup
+
